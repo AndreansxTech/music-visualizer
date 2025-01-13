0d78272fdb4d61446b0ae4012204d49b19c91981
AndreansxTech <koliberekart@icloud.com>
2025-01-13 08:49:33 +0100

    need to make the code look better; deleted all spaces

--- src/js/audioHandler.js
+++ src/js/audioHandler.js
@@ -3,24 +3,53 @@
 let audioContext;
 let analyser;
 let source;
+let audioElement;
 
 function setupAudio() {
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
+audioContext = new (window.AudioContext || window.webkitAudioContext)();
+analyser = audioContext.createAnalyser();
+analyser.fftSize = 2048;
+return audioContext;
 }
 
 function connectAudio(sourceNode) {
-    source = sourceNode;
-    source.connect(analyser);
-    analyser.connect(audioContext.destination);
+source = sourceNode;
+source.connect(analyser);
+analyser.connect(audioContext.destination);
+}
+
+function handleFileInput(file) {
+return new Promise((resolve, reject) => {
+const reader = new FileReader();
+reader.onload = async (event) => {
+try {
+if (!audioContext) {
+setupAudio();
+}
+
+if (audioElement) {
+audioElement.pause();
+}
+
+audioElement = new Audio();
+audioElement.src = event.target.result;
+source = audioContext.createMediaElementSource(audioElement);
+connectAudio(source);
+resolve(audioElement);
+} catch (error) {
+reject(error);
+}
+};
+reader.onerror = reject;
+reader.readAsDataURL(file);
+});
 }
 
 function getAudioData() {
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    analyser.getByteFrequencyData(dataArray);
-    return dataArray;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+analyser.getByteFrequencyData(dataArray);
+return dataArray;
 }
 
-export { setupAudio, connectAudio, getAudioData };
+export { setupAudio, connectAudio, getAudioData, handleFileInput };
